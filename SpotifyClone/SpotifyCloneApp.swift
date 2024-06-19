033a842a940f5a15c025d2339f4d1ff20054c23a
Baisampayan Saha <baisampayans@gmail.com>
2024-06-19 13:36:41 +0530

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Baisampayan Saha on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
